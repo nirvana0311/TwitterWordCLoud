89eb961e97dde797cbbb61c21203663ce9beb600
saurabh khodake <saurabh khodake>
2017-05-06 16:31:14 +0530

    twitter R code checkin

---
+++ TwitterWordCloud.R
@@ -0,0 +1,19 @@
+require(tm)
+require(twitteR)
+require(RCurl)
+Consumer_Key="OPPA5VhYKtsRugJBYyYQvIEWQ"
+Consumer_Secret ="8KAJDzmLog4Tjfwl2bMGPdqftMAN9uHdAxv01frkEZXfjrrjbT"
+Access_Token=	"129692093-51O6nZGHOAluZ77k12wnaFTRi5JfndPb2J5XxyxT"
+Access_Token_Secret=	"0buLfcCCVtNcKlr3YClmdj4gT0cPWgz7o6k1G3JgaFYvn"
+setup_twitter_oauth(consumer_key = Consumer_Key,consumer_secret = Consumer_Secret,access_token = Access_Token,access_secret = Access_Token_Secret)
+chels_tweets=searchTwitter("chelsea",n = 500,lang = "en",resultType = 'recent')
+
+chels_char=sapply(chels_tweets, function(x) x$getText())
+chels_clean=Corpus(VectorSource(chels_char))
+chels_clean=tm_map(chels_clean,removePunctuation)
+chels_clean=tm_map(chels_clean,content_transformer(tolower))
+chels_clean=tm_map(chels_clean,removeWords,stopwords("english"))
+chels_clean=tm_map(chels_clean,removeNumbers)
+chels_clean=tm_map(chels_clean,stripWhitespace)
+chels_clean=tm_map(chels_clean,removeWords,)
+wordcloud(chels_clean,scale = c(3,0.7),random.order = F,colors =brewer.pal(6, "Dark2") )
