19c6fab5424274a1824ce8b5d4c9544503325503
Saurabh Khodake <nirvana.0311@gmail.com>
2017-05-06 16:39:41 +0530

    cosmetic changes

--- TwitterWordCloud.R
+++ TwitterWordCloud.R
@@ -1,3 +1,4 @@
+searchEngineKeyWord="chelsea"
 require(tm)
 require(twitteR)
 require(RCurl)
@@ -6,7 +7,7 @@ Consumer_Secret ="8KAJDzmLog4Tjfwl2bMGPdqftMAN9uHdAxv01frkEZXfjrrjbT"
 Access_Token=	"129692093-51O6nZGHOAluZ77k12wnaFTRi5JfndPb2J5XxyxT"
 Access_Token_Secret=	"0buLfcCCVtNcKlr3YClmdj4gT0cPWgz7o6k1G3JgaFYvn"
 setup_twitter_oauth(consumer_key = Consumer_Key,consumer_secret = Consumer_Secret,access_token = Access_Token,access_secret = Access_Token_Secret)
-chels_tweets=searchTwitter("chelsea",n = 500,lang = "en",resultType = 'recent')
+chels_tweets=searchTwitter(searchEngineKeyWord,n = 500,lang = "en",resultType = 'recent')
 
 chels_char=sapply(chels_tweets, function(x) x$getText())
 chels_clean=Corpus(VectorSource(chels_char))
